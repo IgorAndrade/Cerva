c99cc4dbb8bcab528523aa4e4878a834ca34ee16
Igor Andrade <andrade.igor@gmail.com>
2016-04-05 23:21:55 -0300

    criando busca de cerveja por ID

--- services/cervejaService.js
+++ services/cervejaService.js
@@ -8,14 +8,22 @@ var BreweryDb = require('brewerydb-node');
 var brewdb = new BreweryDb(conf.all.chaves.brewerydb);
 var Service = {
 	pesquisar:function(req,res,next){
-		var q = req.query.q;
-		brewdb.search.beers({ q: q }, function(error,obj,obj2){
+		brewdb.search.beers(req.query, function(error,obj,obj2){
 			if(error)
 				res.status(412).json({ "error": error });
 			else
 				res.status(200).json(obj);
 		});
 	},
+	buscarById:function(req,res,next){
+		brewdb.beer.getById(req.params.id, { withBreweries: "Y" }, function(error,obj,obj2){
+			if(error)
+				res.status(412).json({ "error": error });
+			else
+				res.status(200).json(obj);
+		}); 
+	},
+
 	convertFromBreweryDb:function(lista){
 		var resultado=[];
 		for (var c in lista) {
@@ -36,7 +44,7 @@ var Service = {
 app.post('/services/cerveja', crud.inserir);
 app.get('/services/cerveja', crud.listar);
 app.get('/services/cerveja/pesquisar',app.isLoggedInAjax, Service.pesquisar);
-app.get('/services/cerveja/:id', crud.buscarById);
+app.get('/services/cerveja/:id', Service.buscarById);
 app.put('/services/cerveja/:id', crud.update);
 app.delete('/services/cerveja/:id', crud.deletar);
 
