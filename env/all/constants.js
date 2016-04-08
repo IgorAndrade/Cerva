ed06ea9f3b2b2074ac57cf4b7f90084eafa7bc42
Igor Andrade <andrade.igor@gmail.com>
2016-04-08 01:11:28 -0300

    criando constants

---
+++ env/all/constants.js
@@ -0,0 +1,16 @@
+module.exports = {
+	cerveja:{
+		status:{
+			importado:"importado",
+			verificado:"verificado",
+			pendente:"pendente"
+		},
+		copos:{
+			Goblet:"Cálice",
+			Mug:"Caneca",
+			Tulip:"Tulipa",
+			Stange:"Stange/haste",
+			Weizen:"Trigo"
+		}
+	}
+}
